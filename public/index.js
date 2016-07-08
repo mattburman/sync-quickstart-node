7ad58c2b12cfec689a77f096f6f6735d94c82e10
David Prothero <david@prothero.com>
2016-07-08 09:26:20 -0700

    Tic-Tac-Twilio implementation

--- public/index.js
+++ public/index.js
@@ -1,104 +1,106 @@
-$(function() {
-    //We'll use message to tell the user what's happening
-    var $message = $('#message');
+$(function () {
+  //We'll use message to tell the user what's happening
+  var $message = $('#message');
 
+  //Get handle to the game board buttons
+  var $buttons = $('#board .board-row button');
 
-    // Get handle to the object creation buttons
-    var $syncField = $('#syncField');
+  //Manages the state of our access token we got from the server
+  var accessManager;
 
-    // Manages the state of our access token we got from the server
-    var accessManager;
+  //Our interface to the Sync service
+  var syncClient;
 
-    // Our interface to the Sync service
-    var syncClient;
+  //We're going to use a single Sync document, our simplest
+  //synchronisation primitive, for this demo
+  var syncDoc;
 
-    //We're going to use a single Sync document, our simplest synchronisation primitive, for this demo
-    var syncDoc;
+  //Get an access token for the current user, passing a device ID
+  //For browser-based apps, we'll always just use the
+  //value "browser"
+  $.getJSON('/token', {
+    device: 'browser'
+  }, function (tokenResponse) {
+    //Initialize the Sync client
+    accessManager = new Twilio.AccessManager(tokenResponse.token);
+    syncClient = new Twilio.Sync.Client(accessManager);
 
-    // The server will assign the client a random username - store that value
-    // here
-    var username;
+    //Let's pop a message on the screen to show that Sync is ready
+    $message.html("Sync initialized!");
 
-    // Alert the user they have been assigned a random username
+    //Now that Sync is active, lets enable our game board
+    $buttons.attr("disabled", false);
 
-    // Get an access token for the current user, passing a username (identity)
-    // and a device ID - for browser-based apps, we'll always just use the
-    // value "browser"
-    $.getJSON('/token', {
-        device: 'browser'
-    }, function(data) {
-        // Alert the user they have been assigned a random username
-        // username = data.identity;
+    //This code will create and/or open a Sync document
+    //Note the use of promises
+    syncClient.document("sync.game").then(function(doc) {
+      //Lets store it in our global variable
+      syncDoc = doc;
 
+      //Initialize game board UI to current state
+      writeGameBoard(syncDoc.get());
 
-        // Initialize the IP messaging client
-        accessManager = new Twilio.AccessManager(data.token);
-        syncClient = new Twilio.Sync.Client(accessManager);
-
-        console.log(syncClient);
-
-        //Let's pop a message on the screen to show that Sync is ready
-        $message.html("Sync initialized!");
-
-        //Now that Sync is active, lets enable our text field
-        $syncField.attr("disabled",false);
-
-        //This code will create and/or open a Sync document
-        //Note the use of promises
-        syncClient.document("sync.field").then(doc => {
-          //Lets store it in our global variable
-          syncDoc = doc;
-
-          console.log("SYNCDOC",syncDoc);
-
-          //Let's subscribe to changes on this document, so when something
-          //changes on this document, we can trigger our UI to update
-          syncDoc.on("updated", function(data) {
-
-            console.log(data);
-            $syncField.val(data.fieldValue);
-
-          });
-
-        });
-
+      //Let's subscribe to changes on this document, so when something
+      //changes on this document, we can trigger our UI to update
+      syncDoc.on("updated", writeGameBoard);
 
     });
 
-    //Whenever the input field changes, we want send the value to Sync
-    $syncField.on("keyup",function(e) {
+  });
 
-      //Lets grab the actual form field value
-      var inputValue = $(this).val();
-
-      //and do some super simple validation
-      if(( inputValue === 0) && (inputValue > 500)) {
-        return false;
+  //Update the buttons on the board to match our document
+  function writeGameBoard(data) {
+    for (var row = 0; row < 3; row++) {
+      for (var col = 0; col < 3; col++) {
+        var selector = '[data-row="' + row + '"]' +
+          '[data-col="' + col + '"]';
+        var cellValue = data.board[row][col];
+        $(selector).html(cellValue === '' ? '&nbsp;' : cellValue);
       }
+    }
+  }
 
-      //Lets send the field value to Sync!
-      syncDoc.set({fieldValue:inputValue});
+  //Whenever a board button is clicked:
+  $buttons.on("click", function (e) {
+    //Toggle the value: X, O, or empty
+    toggleCellValue($(e.target));
 
-      //Bonus: Since keyup is so quick to generate traffic,
-      //introducing a slight delay can make it appear less 'jumpy'
-      // delay(function(){
-      //     syncDoc.set({fieldValue:inputValue})
-      //   }, 100 );
+    //Update the document and send to Sync
+    var data = readGameBoard();
+    syncDoc.set(data);
 
-    // syncDoc.set({fieldValue:inputValue});
+  });
 
-      // syncDoc.set({fieldValue:inputValue}).then(val => {
-      //   //$message.html("Synced!").fadeIn(50);
-      // });
+  //Toggle the value: X, O, or empty (&nbsp; for UI)
+  function toggleCellValue($cell) {
+    var cellValue = $cell.html();
 
-      });
+    if (cellValue === 'X') {
+      $cell.html('O');
+    } else if (cellValue === 'O') {
+      $cell.html('&nbsp;');
+    } else {
+      $cell.html('X');
+    }
+  }
+
+  //Read the state of the UI and create a new document
+  function readGameBoard() {
+    var board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ];
+
+    for (var row = 0; row < 3; row++) {
+      for (var col = 0; col < 3; col++) {
+        var selector = '[data-row="' + row + '"]' +
+          '[data-col="' + col + '"]';
+        board[row][col] = $(selector).html().replace('&nbsp;', '');
+      }
+    }
+
+    return {board: board};
+  }
 
 });
-
-var delay = (function(){
-  var timer = 0;
-  return function(callback, ms){
-    clearTimeout (timer);
-    timer = setTimeout(callback, ms);
-  };
-})();
